eb0fdb36780fc140ad659ab0b0038a9757688b21
John Flynn <jflynnpics@gmail.com>
2018-11-28 20:39:48 -0500

    initial build

--- src/App.js
+++ src/App.js
@@ -1,28 +1,107 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios'
+import './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      image: '',
+      message: '',
+      status: '',
+      requests: [
+        '/asshole/cat',
+        '/back/human/cat',
+        '/bag/cat',
+        '/because/cat',
+        '/blackadder/human/cat',
+        '/bucket/cat',
+        '/cocksplat/human/cat',
+        '/cool/cat',
+        '/cup/cat',
+        '/deraadt/human/cat',
+        '/diabetes/cat',
+        '/donut/human/cat',
+        '/equity/human/cat',
+        '/everyone/cat',
+        '/family/cat',
+        '/fascinating/cat',
+        '/flying/cat',
+        '/fts/human/cat',
+        '/fyyff/cat',
+        '/give/cat',
+        '/horse/cat',
+        '/immensity/cat',
+        '/ing/human/cat',
+        '/keep/human/cat',
+        '/king/human/cat',
+        '/life/cat',
+        '/linus/human/cat',
+        '/look/human/cat',
+        '/looking/cat',
+        '/maybe/cat',
+        '/no/cat',
+        '/nugget/human/cat',
+        '/off/human/cat',
+        '/outside/human/cat',
+        '/programmer/cat',
+        '/question/cat',
+        '/retard/cat',
+        '/sake/cat',
+        '/shit/cat',
+        '/shutup/human/cat',
+        '/single/cat',
+        '/thanks/cat',
+        '/that/cat',
+        '/think/human/cat',
+        '/thinking/human/cat',
+        '/this/cat',
+        '/thumbs/human/cat',
+        '/too/cat',
+        '/tucker/cat',
+        '/what/cat',
+        '/xmas/human/cat',
+        '/yoda/human/cat',
+        '/you/human/cat',
+        '/zero/cat'
+      ]
+    }
+  }
+
+  api = () => {
+    axios
+      .get(
+        `https://www.foaas.com${
+          this.state.requests[Math.ceil(Math.random() * 51)]
+        }`
+      )
+      .then(response => {
+        this.setState({
+          message: response.data.message
+        })
+      })
+    axios.get('https://aws.random.cat/meow').then(response => {
+      this.setState({
+        image: response.data.file
+      })
+    })
+    this.setState({
+      status: 'hidden'
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Have you ever wondered what your cat thinks?</h1>
+        <img alt="cat" src={this.state.image} />
+        <p>{this.state.message}</p>
+        <h2 className={this.state.status}>Click the button to find out!</h2>
+        <button onClick={this.api}>Show me what my cat thinks!</button>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/jsonfoass.js
@@ -0,0 +1,1004 @@
+let jsonfoass = [
+  {
+    name: 'Who the fuck are you anyway',
+    url: '/anyway/:company/cat',
+    fields: [
+      {
+        name: 'Company',
+        field: 'company'
+      },
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Asshole',
+    url: '/asshole/cat',
+    fields: [
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Awesome',
+    url: '/awesome/cat',
+    fields: [
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Back the fuck off',
+    url: '/back/human/cat',
+    fields: [
+      {
+        name: 'Name',
+        field: 'name'
+      },
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Bag',
+    url: '/bag/cat',
+    fields: [
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Ballmer',
+    url: '/ballmer/human/:company/cat',
+    fields: [
+      {
+        name: 'Name',
+        field: 'name'
+      },
+      {
+        name: 'Company',
+        field: 'company'
+      },
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Bday',
+    url: '/bday/human/cat',
+    fields: [
+      {
+        name: 'Name',
+        field: 'name'
+      },
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Because',
+    url: '/because/cat',
+    fields: [
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Blackadder',
+    url: '/blackadder/human/cat',
+    fields: [
+      {
+        name: 'Name',
+        field: 'name'
+      },
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Bravo Mike',
+    url: '/bm/human/cat',
+    fields: [
+      {
+        name: 'Name',
+        field: 'name'
+      },
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Bucket',
+    url: '/bucket/cat',
+    fields: [
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Bus',
+    url: '/bus/human/cat',
+    fields: [
+      {
+        name: 'Name',
+        field: 'name'
+      },
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Bye',
+    url: '/bye/cat',
+    fields: [
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Can I Use',
+    url: '/caniuse/:tool/cat',
+    fields: [
+      {
+        name: 'Tool',
+        field: 'tool'
+      },
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Chainsaw',
+    url: '/chainsaw/human/cat',
+    fields: [
+      {
+        name: 'Name',
+        field: 'name'
+      },
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Cocksplat',
+    url: '/cocksplat/human/cat',
+    fields: [
+      {
+        name: 'Name',
+        field: 'name'
+      },
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Cool Story',
+    url: '/cool/cat',
+    fields: [
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Cup',
+    url: '/cup/cat',
+    fields: [
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Dalton',
+    url: '/dalton/human/cat',
+    fields: [
+      {
+        name: 'Name',
+        field: 'name'
+      },
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name:
+      "{name} you are being the usual slimy hypocritical asshole... You may have had value ten years ago, but people will see that you don't anymore.",
+    url: '/deraadt/human/cat',
+    fields: [
+      {
+        name: 'Name',
+        field: 'name'
+      },
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Diabetes',
+    url: '/diabetes/cat',
+    fields: [
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Donut',
+    url: '/donut/human/cat',
+    fields: [
+      {
+        name: 'Name',
+        field: 'name'
+      },
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Do Something',
+    url: '/dosomething/:do/:something/cat',
+    fields: [
+      {
+        name: 'Do',
+        field: 'do'
+      },
+      {
+        name: 'Something',
+        field: 'something'
+      },
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Equity',
+    url: '/equity/human/cat',
+    fields: [
+      {
+        name: 'Name',
+        field: 'name'
+      },
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Everyone',
+    url: '/everyone/cat',
+    fields: [
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Everything',
+    url: '/everything/cat',
+    fields: [
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Family',
+    url: '/family/cat',
+    fields: [
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Fascinating',
+    url: '/fascinating/cat',
+    fields: [
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Field of Fucks',
+    url: '/field/human/cat/:reference',
+    fields: [
+      {
+        name: 'Name',
+        field: 'name'
+      },
+      {
+        name: 'From',
+        field: 'from'
+      },
+      {
+        name: 'Reference',
+        field: 'reference'
+      }
+    ]
+  },
+  {
+    name: 'Flying',
+    url: '/flying/cat',
+    fields: [
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Fuck That Shit',
+    url: '/fts/human/cat',
+    fields: [
+      {
+        name: 'Name',
+        field: 'name'
+      },
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'FYYFF',
+    url: '/fyyff/cat',
+    fields: [
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Golf Foxtrot Yankee',
+    url: '/gfy/human/cat',
+    fields: [
+      {
+        name: 'Name',
+        field: 'name'
+      },
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Give',
+    url: '/give/cat',
+    fields: [
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Greed',
+    url: '/greed/:noun/cat',
+    fields: [
+      {
+        name: 'Noun',
+        field: 'noun'
+      },
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Fuck you and the horse you rode in on',
+    url: '/horse/cat',
+    fields: [
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Immensity',
+    url: '/immensity/cat',
+    fields: [
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Fucking',
+    url: '/ing/human/cat',
+    fields: [
+      {
+        name: 'Name',
+        field: 'name'
+      },
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Keep',
+    url: '/keep/human/cat',
+    fields: [
+      {
+        name: 'Name',
+        field: 'name'
+      },
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Keep Calm',
+    url: '/keepcalm/:reaction/cat',
+    fields: [
+      {
+        name: 'Reaction',
+        field: 'reaction'
+      },
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'King',
+    url: '/king/human/cat',
+    fields: [
+      {
+        name: 'Name',
+        field: 'name'
+      },
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Life',
+    url: '/life/cat',
+    fields: [
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Linus',
+    url: '/linus/human/cat',
+    fields: [
+      {
+        name: 'Name',
+        field: 'name'
+      },
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Look',
+    url: '/look/human/cat',
+    fields: [
+      {
+        name: 'Name',
+        field: 'name'
+      },
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Looking',
+    url: '/looking/cat',
+    fields: [
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Madison',
+    url: '/madison/human/cat',
+    fields: [
+      {
+        name: 'Name',
+        field: 'name'
+      },
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Maybe',
+    url: '/maybe/cat',
+    fields: [
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Fuck Me',
+    url: '/me/cat',
+    fields: [
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'mornin',
+    url: '/mornin/cat',
+    fields: [
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'No',
+    url: '/no/cat',
+    fields: [
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Nugget',
+    url: '/nugget/human/cat',
+    fields: [
+      {
+        name: 'Name',
+        field: 'name'
+      },
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Fuck Off',
+    url: '/off/human/cat',
+    fields: [
+      {
+        name: 'Name',
+        field: 'name'
+      },
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Fuck Off With',
+    url: '/off-with/:behavior/cat',
+    fields: [
+      {
+        name: 'Behavior',
+        field: 'behavior'
+      },
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Outside',
+    url: '/outside/human/cat',
+    fields: [
+      {
+        name: 'Name',
+        field: 'name'
+      },
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'This Thing In Particular',
+    url: '/particular/:thing/cat',
+    fields: [
+      {
+        name: 'Thing',
+        field: 'thing'
+      },
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Pink',
+    url: '/pink/cat',
+    fields: [
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Problem',
+    url: '/problem/human/cat',
+    fields: [
+      {
+        name: 'Name',
+        field: 'name'
+      },
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Programmer',
+    url: '/programmer/cat',
+    fields: [
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Pulp',
+    url: '/pulp/:language/cat',
+    fields: [
+      {
+        name: 'Language',
+        field: 'language'
+      },
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Question',
+    url: '/question/cat',
+    fields: [
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Retard',
+    url: '/retard/cat',
+    fields: [
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: "That's fucking ridiculous",
+    url: '/ridiculous/cat',
+    fields: [
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'RTFM',
+    url: '/rtfm/cat',
+    fields: [
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'sake',
+    url: '/sake/cat',
+    fields: [
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Shakespeare',
+    url: '/shakespeare/human/cat',
+    fields: [
+      {
+        name: 'Name',
+        field: 'name'
+      },
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Fuck This Shit',
+    url: '/shit/cat',
+    fields: [
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Shut Up',
+    url: '/shutup/human/cat',
+    fields: [
+      {
+        name: 'Name',
+        field: 'name'
+      },
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Single',
+    url: '/single/cat',
+    fields: [
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Thanks',
+    url: '/thanks/cat',
+    fields: [
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Fuck That',
+    url: '/that/cat',
+    fields: [
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'You Think',
+    url: '/think/human/cat',
+    fields: [
+      {
+        name: 'Name',
+        field: 'name'
+      },
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Thinking',
+    url: '/thinking/human/cat',
+    fields: [
+      {
+        name: 'Name',
+        field: 'name'
+      },
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Fuck This',
+    url: '/this/cat',
+    fields: [
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'This Guy',
+    url: '/thumbs/human/cat',
+    fields: [
+      {
+        name: 'From',
+        field: 'from'
+      },
+      {
+        name: 'Name',
+        field: 'name'
+      }
+    ]
+  },
+  {
+    name: 'Too',
+    url: '/too/cat',
+    fields: [
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Tucker',
+    url: '/tucker/cat',
+    fields: [
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Version',
+    url: '/version',
+    fields: []
+  },
+  {
+    name: 'What',
+    url: '/what/cat',
+    fields: [
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Xmas',
+    url: '/xmas/human/cat',
+    fields: [
+      {
+        name: 'Name',
+        field: 'name'
+      },
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Yoda',
+    url: '/yoda/human/cat',
+    fields: [
+      {
+        name: 'Name',
+        field: 'name'
+      },
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Fuck You',
+    url: '/you/human/cat',
+    fields: [
+      {
+        name: 'Name',
+        field: 'name'
+      },
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Zayn',
+    url: '/zayn/cat',
+    fields: [
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  },
+  {
+    name: 'Zero',
+    url: '/zero/cat',
+    fields: [
+      {
+        name: 'From',
+        field: 'from'
+      }
+    ]
+  }
+]
